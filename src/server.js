8f4ac6beac4151bae46ceff5ebff85ea25c649b3
AvlisSuetham <matheuspsghx@gmail.com>
2025-12-04 21:16:40 -0300

    update for ejs

--- src/server.js
+++ src/server.js
@@ -4,18 +4,23 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//Debug
+// Configurar EJS como view engine
+app.set('view engine', 'ejs');
+
+// Definir pasta das views relativamente ao src/
+app.set('views', path.join(__dirname, '../views'));
+
+// Debug
 console.log("➡️ __dirname:", __dirname);
 console.log("➡️ Public path:", path.join(__dirname, '../public'));
-console.log("➡️ Views path:", path.join(__dirname, '../views/index.html'));
+console.log("➡️ Views path:", path.join(__dirname, '../views'));
 
-
-// Arquivos estáticos (public)
+// Arquivos estáticos
 app.use(express.static(path.join(__dirname, '../public')));
 
 // Rota principal
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../views/index.html'));
+  res.render('index'); // Renderiza index.ejs
 });
 
 // Inicia o servidor
